97119ef2e05d6bf085f1cdbadda99530d935da80
linda <nmlinda97@gmail.com>
2017-12-02 12:38:51 +0700

    12:38 lnd

--- resources/views/templates/partials/_head.blade.php
+++ resources/views/templates/partials/_head.blade.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="icon" type="image/png" href="{{ asset('assets/dist/img/Logo_Asrama.png') }}">
   <title>Simak Asrama</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
